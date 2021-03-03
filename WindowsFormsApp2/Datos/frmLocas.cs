c8ff7eb863f95416de1bec754286a8227922d0ec
Edisson <eacamacho46@misena.edu.co>
2021-03-03 12:21:08 -0500

    Se agrego formulario locas

---
+++ WindowsFormsApp2/Datos/frmLocas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2.Datos
+{
+    public partial class frmLocas : Form
+    {
+        public frmLocas()
+        {
+            InitializeComponent();
+        }
+    }
+}
